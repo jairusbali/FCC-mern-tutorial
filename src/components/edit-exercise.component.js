c695a1c909ea488e3404c58693eeb129451451c1
Jairus Bali <jairus.bali@gmail.com>
2019-09-19 00:23:25 -0400

    onChange functions implemented for edit-exercise.component

--- src/components/edit-exercise.component.js
+++ src/components/edit-exercise.component.js
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 
-const EditExercise = () => {
-  return <div>Edit Exercise</div>;
-};
+function EditExercise() {
+  const [username, setUsername] = useState("");
+  const [description, setDescription] = useState("");
+  const [duration, setDuration] = useState(0);
+  const [date, setDate] = useState(new Date());
+  const [users, setUsers] = useState([]);
+
+  function onChangeUsername(e) {
+    setUsername(e.target.value);
+  }
+  function onChangeDescription(e) {
+    setDescription(e.target.value);
+  }
+  function onChangeDuration(e) {
+    setDuration(e.target.value);
+  }
+  function onChangeDate(date) {
+    setDate(date);
+  }
+  function onChangeUsers(e) {
+    setUsers(e.target.value);
+  }
+
+  function onSubmit(e) {
+    e.preventDefault();
+
+    const exercise = {
+      username,
+      description,
+      duration,
+      date
+    };
+    console.log(exercise);
+  }
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input type="text" onChange={onChangeUsername} />
+        <input type="text" onChange={onChangeDescription} />
+        <input type="text" onChange={onChangeDuration} />
+        <input type="text" onChange={onChangeDate} />
+        <input type="text" onChange={onChangeUsers} />
+
+        <input type="submit" value="submit" />
+      </form>
+
+      <h1>{username}</h1>
+    </div>
+  );
+}
 
 export default EditExercise;
--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -2,24 +2,24 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      {/* class="navbar navbar-dark bg-dark" */}
-      {/* <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      {/* className="navbar navbar-dark bg-dark" */}
+      {/* <a className="navbar-brand" href="#">Navbar</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
   </button> */}
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-          <a class="nav-item nav-link" href="/">
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          <a className="nav-item nav-link" href="/">
             Exercise List
           </a>
-          <a class="nav-item nav-link active" href="/user">
-            Create User <span class="sr-only">(current)</span>
+          <a className="nav-item nav-link active" href="/user">
+            Create User <span className="sr-only">(current)</span>
           </a>
-          <a class="nav-item nav-link" href="/create">
+          <a className="nav-item nav-link" href="/create">
             Create Exercise
           </a>
-          <a class="nav-item nav-link" href="/edit/:id">
+          <a className="nav-item nav-link" href="/edit/:id">
             Edit Exercise
           </a>
         </div>
