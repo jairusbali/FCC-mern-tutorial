7007d0c514da6d27507e38833ef16e5c966e87fc
Jairus Bali <jairus.bali@gmail.com>
2019-09-22 21:52:22 -0400

    create-user component placeholder

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CreateUser = () => {
-  return <div>Create User</div>;
+  const [user, setUser] = useState("");
+
+  function onChangeUser(e) {
+    setUser(e.target.value);
+  }
+
+  function onSubmit(e) {
+    e.preventDefault();
+
+    console.log("submitting " + user);
+  }
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            className="form-control"
+            id="username"
+            // placeholder="ex: 10"
+            onChange={onChangeUser}
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default CreateUser;
